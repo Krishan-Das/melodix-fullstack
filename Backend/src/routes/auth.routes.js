77a8ece3b7a01314f79679c50f861b411c1c446d
Krishan Das <krishan8974783135@gmail.com>
2026-06-23 23:25:17 +0530

    Login Functionality added.

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 // Register user
 router.post("/register", authController.registerUser);
 
-
+// Login User
+router.post("/login", authController.loginUser);
 
 module.exports = router;
